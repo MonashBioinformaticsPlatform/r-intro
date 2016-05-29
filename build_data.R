836d32268491a24590c59ef729068785508d1486
Paul Harrison <pfh@logarithmic.net>
2016-05-29 11:45:47 +1000

    Add better for loop example

--- build_data.R
+++ build_data.R
@@ -1,7 +1,18 @@
 
 library(faraway)
+library(gapminder)
 library(dplyr)
 library(readr)
+
+data(gapminder)
+
+write_csv(gapminder, "intro-r/all-gapminder.csv")
+
+for(this_year in unique(gapminder$year))
+    gapminder %>%
+    filter(year == this_year) %>%
+    write_csv(paste0("intro-r/gapminder-",this_year,".csv"))
+
 data(diabetes)
 
 dat <- diabetes %>%
@@ -20,5 +31,5 @@ dat2 <- dat %>%
     select(subject, chol) %>%
     filter(runif(n()) < 0.9)
 
-write_csv(dat1, "data/intro-r/diabetes.csv")
-write_csv(dat2, "data/intro-r/chol.csv")
+write_csv(dat1, "intro-r/diabetes.csv")
+write_csv(dat2, "intro-r/chol.csv")
